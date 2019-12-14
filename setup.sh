d3f546112d1f514c6acbb3ccd98720d008f44fb5
Taylor Ono <taylor.r.ono@gmail.com>
2019-12-14 11:46:33 -0800

    Updated bash install script

--- setup.sh
+++ setup.sh
@@ -17,34 +17,39 @@ sudo -K
 sudo true;
 clear
 
+MY_DIR="$(dirname "$0")"
+
+# Update Ubuntu Package Catalog
+sudo apt update -y && sudo apt upgrade -y
+
 source ${MY_DIR}/scripts/common/configuration-bash.sh
 
 # Place any applications that require the user to type in their password here
-brew cask install github
-brew cask install zoomus
-
-source ${MY_DIR}/scripts/common/git.sh
-source ${MY_DIR}/scripts/common/git-aliases.sh
-source ${MY_DIR}/scripts/common/cloud-foundry.sh
-source ${MY_DIR}/scripts/common/applications-common.sh
-source ${MY_DIR}/scripts/common/unix.sh
-source ${MY_DIR}/scripts/common/configuration-osx.sh
-source ${MY_DIR}/scripts/common/configurations.sh
-
-# For each command line argument, try executing the corresponding script in opt-in/
-for var in "$@"
-do
-    echo "$var"
-    FILE=${MY_DIR}/scripts/opt-in/${var}.sh
-    echo "$FILE"
-    if [ -f $FILE ]; then
-        source ${FILE}
-    else
-       echo "Warning: $var does not appear to be a valid argument. File $FILE does not exist."
-    fi
-done
-
-source ${MY_DIR}/scripts/common/finished.sh
-if (( SKIP_ANALYTICS == 0 )); then
-    source ${MY_DIR}/scripts/helpers/google-analytics.sh ${clientID} finish $@
-fi
+#brew cask install github
+#brew cask install zoomus
+#
+#source ${MY_DIR}/scripts/common/git.sh
+#source ${MY_DIR}/scripts/common/git-aliases.sh
+#source ${MY_DIR}/scripts/common/cloud-foundry.sh
+#source ${MY_DIR}/scripts/common/applications-common.sh
+#source ${MY_DIR}/scripts/common/unix.sh
+#source ${MY_DIR}/scripts/common/configuration-osx.sh
+#source ${MY_DIR}/scripts/common/configurations.sh
+#
+## For each command line argument, try executing the corresponding script in opt-in/
+#for var in "$@"
+#do
+#    echo "$var"
+#    FILE=${MY_DIR}/scripts/opt-in/${var}.sh
+#    echo "$FILE"
+#    if [ -f $FILE ]; then
+#        source ${FILE}
+#    else
+#       echo "Warning: $var does not appear to be a valid argument. File $FILE does not exist."
+#    fi
+#done
+#
+#source ${MY_DIR}/scripts/common/finished.sh
+#if (( SKIP_ANALYTICS == 0 )); then
+#    source ${MY_DIR}/scripts/helpers/google-analytics.sh ${clientID} finish $@
+#fi
