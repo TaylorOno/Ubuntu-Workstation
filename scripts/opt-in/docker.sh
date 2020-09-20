3d0682a3a0ad4304ab75011d1636448d050af767
TaylorOno <taylor.r.ono@gmail.com>
2020-09-20 14:02:06 -0700

    Added docker compose

--- scripts/opt-in/docker.sh
+++ scripts/opt-in/docker.sh
@@ -6,10 +6,12 @@ set +e
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose
 
 sudo curl -L https://raw.githubusercontent.com/docker/compose/master/contrib/completion/bash/docker-compose -o /etc/bash_completion.d/docker-compose
 sudo curl -L https://raw.githubusercontent.com/docker/docker-ce/master/components/cli/contrib/completion/bash/docker -o /etc/bash_completion.d/docker
 sudo curl -L https://raw.githubusercontent.com/docker/machine/master/contrib/completion/bash/docker-machine.bash -o /etc/bash_completion.d/docker-machine
+sudo groupadd docker
+sudo usermod -aG docker ${USER}
 
 set -e
