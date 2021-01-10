04411b5e3c5094afade1ad8e2ab4b4fb10779a14
TaylorOno <taylor.r.ono@gmail.com>
2021-01-09 17:24:23 -0800

    Added opt-in terraform

---
+++ scripts/opt-in/terraform.sh
@@ -0,0 +1,6 @@
+curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+
+sudo apt-add-repository "deb [arch=$(dpkg --print-architecture)] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+sudo apt-get update
+sudo apt install -y terraform
+terraform -install-autocomplete
