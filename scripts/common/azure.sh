91b65c64e3082fd883b04e32c10ee646f35117c2
TaylorOno <taylor.r.ono@gmail.com>
2021-01-09 22:08:57 -0800

    Added Azure CLI

---
+++ scripts/common/azure.sh
@@ -0,0 +1,4 @@
+curl -sL https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor | sudo apt-key add -
+sudo apt-add-repository "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $(lsb_release -cs) main"
+sudo apt-get update
+sudo apt install azure-cli
