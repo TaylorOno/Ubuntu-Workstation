141d44304e1757d06584e56c63bf678782e2960d
Taylor Ono <Taylor.R.Ono@gmail.com>
2021-05-31 18:25:12 -0700

    Added helm opt-in

---
+++ scripts/opt-in/helm.sh
@@ -0,0 +1,18 @@
+echo
+echo "Installing common helm tooling"
+
+curl https://baltocdn.com/helm/signing.asc | sudo apt-key add -
+echo "deb https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
+sudo apt-get update
+sudo apt-get install -y helm
+
+# Shell completion
+FILE=~/.bashrc
+if [[ -f "$FILE" ]]; then
+    echo "$FILE exists proceeding."
+else
+    echo "$FILE does not exist, creating."
+    touch $FILE
+fi
+
+echo 'source <(helm completion bash)' >>~/.bashrc
